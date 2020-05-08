7b91fe476c2d49d92e8d964602a764dff55d230e
Afilnor <f.dauphin@eulerian.com>
2020-05-08 21:48:42 +0200

    0.0.2 conf files

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
-from distutils.core import setup
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name = 'eulerian_analytics',
-    packages = ['eulerian_analytics'],
-    version = '0.1',
-    description = 'A python 3 module to download locally a datamining dataset from the Eulerian Technologies API',
     author = 'Florian',
     author_email = 'f.dauphin@eulerian.com',
-    url = 'https://github.com/Afilnor/Eulerian-Analytics',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent"
+    ],
+    description = 'Locally download a datamining dataset from the Eulerian Technologies API',
     download_url = 'https://github.com/Afilnor/Eulerian-Analytics/archive/master.zip',
+    install_requires=['requests>=2.23.0'],
     keywords = ['eulerian', 'datamining', 'download'],
-    classifiers = [],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name = 'eulerian_analytics',
+    packages = ['eulerian_analytics'],
+    platforms = ['any'],
+    python_requires='>=3',
+    url = 'https://github.com/Afilnor/Eulerian-Analytics',
+    version = '0.0.2',
 )
