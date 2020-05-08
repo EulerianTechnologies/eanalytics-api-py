08bb157c165fc0d7235e5aa8355f59cb3253eb14
florian <f.dauphin@eulerian.com>
2020-05-08 09:43:29 +0200

    initial setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'eulerian_analytics',
+    packages = ['eulerian_analytics'],
+    version = '0.1',
+    description = 'description',
+    author = 'Florian',
+    author_email = 'f.dauphin@eulerian.com',
+    url = 'https://github.com/Afilnor/Eulerian-Analytics',
+    download_url = 'https://github.com/Afilnor/Eulerian-Analytics/archive/master.zip',
+    keywords = ['eulerian', 'datamining', 'download'],
+    classifiers = [],
+)
